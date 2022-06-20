b9c3855545be49623bae1e37b8e9b2a14528b64f
Devesh <Devesh.lakwal911@gmail.com>
2022-06-20 20:59:29 +0530

    Final_sumission

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,9 +39,9 @@ export default function TextForm(props) {
     <div className={`container text-${props.mode ==='light'?'dark':'light'} `}> 
       <h1>{props.heading}</h1>
       <div className="mb-3 text-light">
-      <textarea className="form-control" label="enter" id="exampleFormControlTextarea1" value={text} onChange={handleonchange} style={{backgroundColor: props.mode==='dark'?'rgb(22,25,27)':'white'}} rows="3"></textarea>
-      <textarea className="form-control" id="exampleFormControlTextarea1" value={fWord} onChange={handlefindChange} style={{backgroundColor: props.mode==='dark'?'rgb(22,25,27)':'white'}} rows="3"></textarea>
-      <textarea className="form-control" id="exampleFormControlTextarea1" value={rWord} onChange={handleReplaceChange} style={{backgroundColor: props.mode==='dark'?'rgb(22,25,27)':'white'}} rows="3"></textarea>
+      <textarea className="form-control" label="enter" id="exampleFormControlTextarea1" value={text} onChange={handleonchange} style={{color: props.mode==='light'?'black':'#816797' , backgroundColor:props.mode==='light'?'white':'#1B2430'}} rows="3"></textarea>
+      <textarea className="form-control" id="exampleFormControlTextarea1" value={fWord} onChange={handlefindChange} style={{color: props.mode==='light'?'black':'#816797' , backgroundColor:props.mode==='light'?'white':'#1B2430'}} rows="3"></textarea>
+      <textarea className="form-control" id="exampleFormControlTextarea1" value={rWord} onChange={handleReplaceChange} style={{color: props.mode==='light'?'black':'#816797' , backgroundColor:props.mode==='light'?'white':'#1B2430'}} rows="3"></textarea>
     </div>
     <button className="btn btn-primary" onClick={handleUpClick} >To uppercase</button>
     <button className="btn btn-primary mx-2" onClick={handleLowClick} >To lowercase</button>
@@ -49,7 +49,7 @@ export default function TextForm(props) {
 
   
   </div>
-  <div className="className mx-2 " style={{color: props.mode==='light'?'black':'#816797' }} >
+  <div className="className mx-2 " style={{color: props.mode==='light'?'black':'#816797'}} >
     <h1>Your text summary</h1>
     <p>{text.split(" ").length } word and {text.length} charachters</p>
     <p>{0.08 *text.split(" ").length } time to read</p>
